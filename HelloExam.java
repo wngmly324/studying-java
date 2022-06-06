b686c406ce551ff925f3b95c4e81ae5bf4e94bea
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-06 20:44:25 +0900

    ch2 q3

---
+++ HelloExam.java
@@ -0,0 +1,13 @@
+package java_exam;
+
+public class HelloExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloExam!!");
+		System.out.println("HelloExam!!");
+		System.out.println("HelloExam!!");
+		
+	}
+
+}
