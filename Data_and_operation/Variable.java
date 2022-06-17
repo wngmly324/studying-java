2daac49f2dac4eec759d6db0827c37e205552ccc
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-17 21:08:18 +0900

    add Variable

---
+++ Data_and_operation/Variable.java
@@ -0,0 +1,22 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int a = 1; // number -> integer ... -2, -1, 0, 1, 2 ...
+		System.out.println(a);
+		
+		double b = 1.1; // real number -> double ... -2.0, -1.0, 0, 1.0, 2.0 ...
+		System.out.println(b);
+		
+		String c = "Hello World";
+		System.out.println(c);
+
+	}
+
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA1 - 8.1. 변수의 정의", https://www.youtube.com/watch?v=FtWGiWBNJ-I&t=34s
+ */
