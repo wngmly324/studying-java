bc4e35352c146898d7d1b01244e3efee7aff0689
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-17 21:35:39 +0900

    add Program

---
+++ Program.java
@@ -0,0 +1,17 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+
+	}
+
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA1 - 9.1. 프로그래밍이란 무엇인가?", https://www.youtube.com/watch?v=ejPBOti5omY
+ */
