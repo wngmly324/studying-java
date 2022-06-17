b68e6308c9a1dff49e1b41b4b98635c8b2be1e9f
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-17 21:08:33 +0900

    add Letter

---
+++ Data_and_operation/Letter.java
@@ -0,0 +1,18 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name = "leezche";
+		System.out.println("Hello, " + name + " ... " + name + " ... egoing ... bye");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+		
+	}
+
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA1 - 8.2. 변수의 효용", https://www.youtube.com/watch?v=GUpmH11BF2Y
+ */
