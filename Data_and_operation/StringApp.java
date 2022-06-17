6a42dda4842cdcb462dd8a098425862ec673fa2d
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-17 20:33:54 +0900

    add StringApp

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,27 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		// Character VS String
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character
+		System.out.println("H"); // String
+		
+		System.out.println("Hello "
+				+ "World");
+		
+		// new line
+		System.out.println("Hello \nWorld");
+
+		// escape
+		System.out.println("Hello \"World\""); //Hello "World"
+
+	}
+
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA1 - 6.4. 문자열의 표현", https://www.youtube.com/watch?v=DCa3qo8Pm6E&t=38s
+ */
