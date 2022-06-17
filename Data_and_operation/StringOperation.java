f6427fbcf404e7a8eeadbbb8a88deded22c6c6a2
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-17 20:34:19 +0900

    add StringOperation

---
+++ Data_and_operation/StringOperation.java
@@ -0,0 +1,16 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World".length()); // 11
+		System.out.println("Hello, [[[name]]] ... bye.".replace("[[[name]]]", "duru"));
+
+	}
+
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA1 - 6.5. 문자열 다루기", https://www.youtube.com/watch?v=y20Fhlseh4w&t=274s
+ */
