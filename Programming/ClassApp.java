1a8c64ee19c7df7a52c36596a69bb1cb9f96c92c
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-20 16:13:02 +0900

    add ClassApp

---
+++ Programming/ClassApp.java
@@ -0,0 +1,17 @@
+
+public class ClassApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.6)); //내림
+		System.out.println(Math.ceil(1.6)); //올림
+
+	}
+
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA1 - 13.3. 자바 문서 보는 법 - 클래스", https://www.youtube.com/watch?v=5z5OBTWLeQY
+ */
