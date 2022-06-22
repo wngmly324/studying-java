a057c551b261a3981b452c659081388cc1cb9b77
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-22 15:34:30 +0900

    add BooleanApp

---
+++ BooleanApp.java
@@ -0,0 +1,25 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("One");
+		System.out.println(1);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello world";
+//		String true = "Hello World"; reserved word
+		
+		System.out.println(foo.contains("world"));
+		System.out.println(foo.contains("egoing"));
+
+	}
+
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA 제어문 - 2. Boolean Datatype", https://www.youtube.com/watch?v=9fVFYSYIyyw
+ */
