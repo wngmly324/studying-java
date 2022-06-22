853b4a93f386ac6ca2c50eca3ce1e8bea6e7aaa8
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-22 15:36:13 +0900

    add IfApp

---
+++ java-flow-control/IfApp.java
@@ -0,0 +1,35 @@
+
+public class IfApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("a");
+//		if(false) {
+//			System.out.println(1);
+//		} else {
+//			if(true) {
+//				System.out.println(2);
+//			} else {
+//				System.out.println(3);
+//			}
+//			
+//		}
+		
+		if(false) {
+			System.out.println(1);
+		} else if(true) {
+			System.out.println(2);
+		} else {
+			System.out.println(3);
+		}
+		
+		System.out.println("b");
+
+	}
+
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA 제어문 - 4.1. 조건문 형식", https://www.youtube.com/watch?v=A_2Gl055OSc
+ */
