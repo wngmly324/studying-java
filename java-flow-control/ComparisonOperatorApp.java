44062a0d53b8f710cb9b314f41de818c04302752
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-22 15:35:37 +0900

    add ComparisonOperatorApp

---
+++ java-flow-control/ComparisonOperatorApp.java
@@ -0,0 +1,18 @@
+
+public class ComparisonOperatorApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1 > 1); //false
+		System.out.println(1 == 1); // true
+		System.out.println(1 < 1); //false
+		System.out.println(1 >= 1); //true
+
+	}
+
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA 제어문 - 3. 비교연산자", https://www.youtube.com/watch?v=3TZ8k3VX87A&t=2s
+ */
