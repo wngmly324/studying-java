821ce846d23b3e1c23dbadaa761fac6b57d9eed5
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-23 18:25:37 +0900

    add FirstMethod

---
+++ FirstMethod.java
@@ -0,0 +1,16 @@
+
+public class FirstMethod {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Method");
+		System.out.println(Math.floor(1.1));
+
+	}
+
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA method - 2. 이미 익숙한 메소드", https://www.youtube.com/watch?v=ljHzQwL02LM
+ */
