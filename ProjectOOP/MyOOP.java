b45ed0ffac239878def157195b03cda926062af9
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-06-24 15:45:15 +0900

    add OOP2

---
+++ ProjectOOP/MyOOP.java
@@ -0,0 +1,34 @@
+public class MyOOP {
+
+	public static void main(String[] args) {
+		
+		Print p1 = new Print();
+		p1.delimiter = "----";
+		p1.A();
+		p1.A();
+		p1.B();
+		p1.B();
+		
+		Print p2 = new Print();
+		p2.delimiter = "****";
+		p2.A();
+		p2.A();
+		p2.B();
+		p2.B();
+		
+		p1.A();
+		p2.A();
+		p1.A();
+		p2.A();
+		
+	}
+
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA 객체 지향 프로그래밍 - 3. 변수와 메소드", https://www.youtube.com/watch?v=YcWaITX54k4
+ * 생활코딩, "JAVA 객체 지향 프로그래밍 - 4.1. 클래스 - 존재 이유와 기본형식", https://www.youtube.com/watch?v=m1Cx8vDDmYo&t=2s
+ * 생활코딩, "JAVA 객체 지향 프로그래밍 - 4.2. 클래스 - 형식", https://www.youtube.com/watch?v=jpcXlhgEzmQ
+ * 생활코딩, "JAVA 객체 지향 프로그래밍 - 5. 인스턴스", https://www.youtube.com/watch?v=Y370ydbIb7Y
+ */
---
+++ ProjectOOP/Print.java
@@ -0,0 +1,23 @@
+
+class Print {
+	// static은 클래스의 소속
+	
+	public String delimiter = "";
+
+	public void A() {
+		System.out.println(delimiter);
+		System.out.println("A");
+		System.out.println("A");
+	}
+	public void B() {
+		System.out.println(delimiter);
+		System.out.println("B");
+		System.out.println("B");
+	}
+}
+
+/*
+ * 출처
+ * 생활코딩, "JAVA 객체 지향 프로그래밍 - 4.2. 클래스 - 형식", https://www.youtube.com/watch?v=jpcXlhgEzmQ
+ * 생활코딩, "JAVA 객체 지향 프로그래밍 - 5. 인스턴스", https://www.youtube.com/watch?v=Y370ydbIb7Y
+ */
